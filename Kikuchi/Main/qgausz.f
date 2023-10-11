b0fe43c7be9b0b074ed9886db235928260d137d4
David Duran <dadch1404@gmail.com>
2023-10-11 07:13:27 -0500

    fix: :bug: missing deps

---
+++ Kikuchi/Main/qgausz.f
@@ -0,0 +1,18 @@
+SUBROUTINE qgausz(zfunc, a, b, ss)
+    REAL a, b, ss, zfunc
+    EXTERNAL zfunc
+    INTEGER j
+    REAL dx, xm, xr, w(5), x(5)
+    SAVE w, x
+    DATA w/0.2955242247, .2692667193, .2190863625, .1494513491, .0666713443/
+    DATA x/.1488743389, .4333953941, .6794095682, .8650633666, .9739065285/
+    xr=0.5*(b-a)
+    xm=0.5*(b+a)
+    ss=0
+    DO j = 1, 5
+        dx=xr*x(j)
+        ss = ss + w(j)*(func(xm+dx) + func(xm-dx))
+    ENDDO
+    ss=xr*ss
+    RETURN
+END
