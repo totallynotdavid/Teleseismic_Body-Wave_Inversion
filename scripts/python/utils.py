fdf4aa33f221dd35b653b5f1447492a5e7568ba7
David Duran <dadch1404@gmail.com>
2023-11-06 06:43:44 -0500

    Update .gitignore and delete utils.py

--- scripts/python/utils.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-
-def obtener_ubicacion_repo():
-    """
-    Retorna el path absoluto al directorio raíz del repositorio.
-    """
-    return subprocess.check_output(["git", "rev-parse", "--show-toplevel"]).strip().decode('utf-8')
